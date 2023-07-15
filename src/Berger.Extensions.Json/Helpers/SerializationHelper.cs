8c15f8d0c5d413a49f85dbb7d01e4405d8424d63
Rafael Cronemberger <rafael@berger.com.br>
2023-07-15 15:36:15 -0300

    Adding new yamls for deployment testing

---
+++ src/Berger.Extensions.Json/Helpers/SerializationHelper.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace Berger.Extensions.Json
+{
+    public static class SerializationHelper
+    {
+        public static string Serialize(this object value)
+        {
+            return JsonConvert.SerializeObject(value, Formatting.Indented);
+        }
+        public static string Serialize(this string value)
+        {
+            return JsonConvert.SerializeObject(value, Formatting.Indented);
+        }
+        public static async Task<T> Deserialize<T>(this HttpResponseMessage response)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+
+            return Deserialize<T>(content);
+        }
+        public static T Deserialize<T>(this string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+        public static object Deserialize(this string json)
+        {
+            return JsonConvert.DeserializeObject(json);
+        }
+    }
+}
