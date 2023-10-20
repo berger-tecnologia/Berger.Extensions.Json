3515802efb32e9cf5520d5675c222f121e3fa889
Rafael Cronemberger <rafael@berger.com.br>
2023-10-20 00:48:18 -0300

    Package updates

--- src/Berger.Extensions.Newtonsoft/Helpers/SerializationHelper.cs
+++ src/Berger.Extensions.Newtonsoft/Helpers/SerializationHelper.cs
@@ -4,13 +4,13 @@ namespace Berger.Extensions.Newtonsoft
 {
     public static class SerializationHelper
     {
-        public static string Serialize(this object value)
+        public static string Serialize(this object value, Formatting format = Formatting.Indented)
         {
-            return JsonConvert.SerializeObject(value, Formatting.Indented);
+            return JsonConvert.SerializeObject(value, format);
         }
-        public static string Serialize(this string value)
+        public static string Serialize(this string value, Formatting format = Formatting.Indented)
         {
-            return JsonConvert.SerializeObject(value, Formatting.Indented);
+            return JsonConvert.SerializeObject(value, format);
         }
         public static async Task<T> Deserialize<T>(this HttpResponseMessage response)
         {
@@ -22,9 +22,17 @@ namespace Berger.Extensions.Newtonsoft
         {
             return JsonConvert.DeserializeObject<T>(json);
         }
+        public static T Deserialize<T>(this string json, JsonSerializerSettings settings)
+        {
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
         public static object Deserialize(this string json)
         {
             return JsonConvert.DeserializeObject(json);
+        }
+        public static object Deserialize(this string json, JsonSerializerSettings settings)
+        {
+            return JsonConvert.DeserializeObject(json, settings);
         }
     }
 }
